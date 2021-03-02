956b5abfef69c087933346d21bda7237344684d0
danilihsanov <danilihsanov@localhost>
2021-03-02 12:02:25 +0300

    add main logic

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -1,20 +1,28 @@
 package com.tsystems.javaschool.tasks.subsequence;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Subsequence {
 
-    /**
-     * Checks if it is possible to get a sequence which is equal to the first
-     * one by removing some elements from the second one.
-     *
-     * @param x first sequence
-     * @param y second sequence
-     * @return <code>true</code> if possible, otherwise <code>false</code>
-     */
-    @SuppressWarnings("rawtypes")
-    public boolean find(List x, List y) {
-        // TODO: Implement the logic here
-        return false;
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public boolean find(List firstSequence, List secondSequence) {
+        int secondSequencePos = 0;
+        
+        final List list = new ArrayList(firstSequence);
+        
+        for (Object firstSequenceElem : firstSequence) {
+            while (secondSequencePos < secondSequence.size()) {
+                if (firstSequenceElem.equals(secondSequence.get(secondSequencePos))) {
+                    list.remove(firstSequenceElem);
+                    break;
+                }
+                
+                secondSequencePos++;
+            }
+        }
+        
+        return list.isEmpty();
     }
+    
 }
